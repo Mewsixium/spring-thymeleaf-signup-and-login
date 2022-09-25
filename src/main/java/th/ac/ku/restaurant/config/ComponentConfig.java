f3f7144118e8ce3f28a0f98d6e59a85aec069531
Warunee Benjanan <warunee.be@ku.th>
2022-09-25 17:18:41 +0700

    Spring Thymeleaf validation, connect API, role

--- src/main/java/th/ac/ku/restaurant/config/ComponentConfig.java
+++ src/main/java/th/ac/ku/restaurant/config/ComponentConfig.java
@@ -1,2 +1,23 @@
-package th.ac.ku.restaurant.config;public class ComponentConfig {
+package th.ac.ku.restaurant.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ComponentConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
 }
+
